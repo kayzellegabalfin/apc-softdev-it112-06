c03b26d5640745ed5d89929bc15fa34439cc85d6
Kayzelle Gabalfin <kayzellegabalfin@gmail.com>
2015-02-11 08:28:02 +0800

    Added practice/rbagenconsultancyservices

---
+++ practice/rbagenconsultancyservices/views/site/index.php
@@ -0,0 +1,83 @@
+<?php 
+use yii\helpers\Html;
+
+$this->title = 'RBA General Consultancy and Services | Client Servicing System';
+?>
+
+			
+                
+            <div class="top-nav">
+                <ul>
+                    <li class="active"><a href="./index.php">Home</a></li>
+                    <li><a href="./index.php?r=site%2Fabout">About</a></li>
+                    <li><a href="./index.php?r=site%2Fservices">Services</a></li>
+                    <li><a href="./index.php?r=site%2Fcontact">Contact</a></li>
+                </ul>
+			</div>
+
+<!---start-content----->				
+	<div class="image-slider">
+			<ul class="rslides" id="slider1">
+				<li><img src="images/slider1.png" alt=""></li>
+				<li><img src="images/slider2.png" alt=""></li>
+				<li><img src="images/slider3.png" alt=""></li>
+			</ul>
+		</div>
+	</div>
+</div>	
+
+<div class="content">
+	<div class="border"></div>
+
+	<div class="wel-mesg">
+		<div class="wrap">
+			<h2>Welcome to our site!</h2>
+			<p>RBA Consultancy and General Services processes different kinds of travel Visa of expatriate here in the Philippines.</p>
+			<p>We are dedicated to give our clients like you fast but economical way to process your Visa and other legal documents.</p>
+			<a href="#">Join us!</a>
+		</div>
+	</div>
+
+	<div class="border"></div>
+
+	<div class="content-top-grids">
+		<div class="wrap">
+			<div class="clear"> </div>
+		</div>
+	</div>
+	
+	<div class="border"></div>
+			
+	<div class="content-banner">
+		<div class="wrap">
+			<div class="clear"> </div>
+		</div>
+	</div>
+	
+	<div class="border"></div>
+	
+	<div class="bottom-grids">
+		<div class="wrap">
+			<div class="clear"> </div>
+		</div>
+		
+		<div class="clear"> </div>
+	</div>
+<!--end-content--->
+
+     <!---start-footer--->
+	 <div class="border">
+			</div>
+			<div class="footer">
+				<div class="wrap">
+					<div class="footer-left">
+						<a href="index.php"><img src="images/logo1.png" alt="" /></a>
+					</div>
+					<div class="footer-right">
+						<a href="#top" class="scroll"> </a>
+					</div>
+					<div class="clear"> </div>
+				</div>
+			</div>
+		</div>
+       <!---end-footer--->
