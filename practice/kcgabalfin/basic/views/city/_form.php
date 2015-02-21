0940024d464c876b9a110cca841405b530c0deb2
Kayzelle Gabalfin <kayzellegabalfin@gmail.com>
2015-02-21 08:32:39 +0800

    Created dropDownList for Province and City Classes

--- practice/kcgabalfin/basic/views/city/_form.php
+++ practice/kcgabalfin/basic/views/city/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Province;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\City */
@@ -15,8 +17,16 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'city_code')->textInput(['maxlength' => 32]) ?>
 
     <?= $form->field($model, 'city_description')->textInput(['maxlength' => 32]) ?>
+	
+	<?php 
+		$province=province::find()->all();
 
-    <?= $form->field($model, 'province_id')->textInput() ?>
+		$listData=ArrayHelper::map($province,'id','province_code');
+
+		echo $form->field($model, 'province_id')->dropDownList(
+								$listData, 
+								['prompt'=>'Select province...']);
+	?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
