9d1b868d8bf4d525d5591924efb6e8aa4eb86fdb
Kayzelle Gabalfin <kayzellegabalfin@gmail.com>
2015-02-28 09:22:31 +0800

    Added dropDownList for myaddress_id field

--- practice/midterm_test/Gabalfin_Kayzelle_midterms/ourspace/views/mycomment/_form.php
+++ practice/midterm_test/Gabalfin_Kayzelle_midterms/ourspace/views/mycomment/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Myaddress;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Mycomment */
@@ -14,7 +16,15 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'id')->textInput() ?>
 
-    <?= $form->field($model, 'myaddress_id')->textInput() ?>
+	<?php 
+		$Myaddress=Myaddress::find()->all();
+
+		$listData=ArrayHelper::map($Myaddress,'id','lastname');
+
+		echo $form->field($model, 'myaddress_id')->dropDownList(
+								$listData, 
+								['prompt'=>'Select Myaddress...']);
+	?>
 
     <?= $form->field($model, 'author')->textInput(['maxlength' => 255]) ?>
 
