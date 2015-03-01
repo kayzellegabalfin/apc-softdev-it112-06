a7a387af95aa92014ebd3c9155bc33761776a073
Troi Mendoza <troiraymund@gmail.com>
2015-03-01 16:03:46 +0800

    Added database connection to softdev_troi_raymund_ourspace

--- practice/midterm_test/Mendoza_Troi_Raymund_midterms/ourspace/config/db.php
+++ practice/midterm_test/Mendoza_Troi_Raymund_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_mendoza_troi_raymund_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
