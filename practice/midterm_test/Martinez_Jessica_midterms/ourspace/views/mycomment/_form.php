69a8e17c016df7b59fc9a3a4876285e933c0562f
Jessica Martinez <martinezjecca@gmail.com>
2015-02-28 16:54:36 -0800

    Added dropDownList for myaddress_id field

--- practice/midterm_test/Martinez_Jessica_midterms/ourspace/views/mycomment/_form.php
+++ practice/midterm_test/Martinez_Jessica_midterms/ourspace/views/mycomment/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Myaddress;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\mycomment */
@@ -14,13 +16,27 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'id')->textInput() ?>
 
-    <?= $form->field($model, 'myaddress_id')->textInput() ?>
+    	
+	<?php
+	$myaddress=myaddress::find()->all();
+
+	$listData=ArrayHelper::map($myaddress,'id','lastname');
+
+ 
+	echo $form->field($model, 'myaddress')->dropDownList(
+                                $listData,
+
+                                ['prompt'=>'Select Lastname...']);
+
+?>
 
     <?= $form->field($model, 'author')->textInput(['maxlength' => 255]) ?>
 
     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'created_at')->textInput() ?>
+	
+	
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
