020a3a28630980cc55b002ae2571c42557a6e0e6
Kayzelle Gabalfin <kayzellegabalfin@gmail.com>
2015-03-03 17:27:46 +0800

    Updated CSS for Services web page

--- application/rbaconsultancy/views/site/services.php
+++ application/rbaconsultancy/views/site/services.php
@@ -37,55 +37,55 @@
 				<div class="content">
 					<section id="section-1">
 						<div class="mediabox">
-							<h3>Immigrant Visa 1</h3>
-							<p>Sushi gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.</p>
+							<a href="#">Conversion to Non-Quota Immigrant by Marriage</a>
+							<p>For a foreign national on the basis of his valid marriage to a Philippine citizen.</p>
 						</div>
 						<div class="mediabox">
-							<h3>Immigrant Visa 2</h3>
+							<a href="#">Immigrant Visa 2</a>
 							<p>Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea.</p>
 						</div>
 						<div class="mediabox">
-							<h3>Immigrant Visa 3</h3>
+							<a href="#">Immigrant Visa 3</a>
 							<p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
 						</div>
 						<div class="mediabox">
-							<h3>Immigrant Visa 4</h3>
+							<a href="#">Immigrant Visa 4</a>
 							<p>Sushi gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.</p>
 						</div>
 						<div class="mediabox">
-							<h3>Immigrant Visa 5</h3>
+							<a href="#">Immigrant Visa 5</a>
 							<p>Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea.</p>
 						</div>
 						<div class="mediabox">
-							<h3>Immigrant Visa 6</h3>
+							<a href="#">Immigrant Visa 6</a>
 							<p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
 						</div>
 					</section>
 					<section id="section-2">
 						<div class="mediabox">
-							<h3>Non-Immigrant Visa 1</h3>
-							<p>Chickweed okra pea winter purslane coriander yarrow sweet pepper radish garlic brussels sprout groundnut summer purslane earthnut pea tomato spring onion azuki bean gourd. </p>
+							<a href="#">Extension of Pre-arranged Employee (Commercial)</a>
+							<p>For foreign nationals who are existing holders of a Pre-arranged Employee – Commercial Visa and wishes to continue to his employment in the Philippines</p>
 						</div>
 						<div class="mediabox">
-							<h3>Non-Immigrant Visa 2</h3>
+							<a href="#">Non-Immigrant Visa 2</a>
 							<p>Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori. Grape wattle seed kombu beetroot horseradish carrot squash brussels sprout chard.</p>
 						</div>
 						<div class="mediabox">
-							<h3>Non-Immigrant Visa 3</h3>
+							<a href="#">Non-Immigrant Visa 3</a>
 							<p>Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori. </p>
 						</div>
 					</section>
 					<section id="section-3">
 						<div class="mediabox">
-							<h3>Special Visa 1</h3>
+							<a href="#">Special Visa 1</a>
 							<p>Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea.Sushi gumbo beet greens corn soko endive gumbo gourd.</p>
 						</div>
 						<div class="mediabox">
-							<h3>Special Visa 2</h3>
+							<a href="#">Special Visa 2</a>
 							<p>Sushi gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.</p>
 						</div>
 						<div class="mediabox">
-							<h3>Special Visa 3</h3>
+							<a href="#">Special Visa 3</a>
 							<p>Pea horseradish azuki bean lettuce avocado asparagus okra. Kohlrabi radish okra azuki bean corn fava bean mustard tigernut wasabi tofu broccoli mixture soup.</p>
 						</div>
 					</section>
