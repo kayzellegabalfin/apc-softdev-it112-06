160027176c3961f1caedb812bd98392b20bf6808
Kayzelle Gabalfin <kayzellegabalfin@gmail.com>
2015-03-03 13:46:00 +0800

    Created Services web page

---
+++ application/rbaconsultancy/views/site/services.php
@@ -0,0 +1,98 @@
+                
+            <div class="top-nav">
+                <ul>
+                    <li><a href="./index.php">Home</a></li>
+                    <li><a href="./index.php?r=site%2Fabout">About</a></li>
+                    <li class="active"><a href="./index.php?r=site%2Fservices">Services</a></li>
+                    <li><a href="./index.php?r=site%2Fcontact">Contact</a></li>
+                </ul>
+            </div>
+	</div>
+</div>	
+		<link rel="stylesheet" type="text/css" href="css/tabs.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+
+		<div class="about">
+		<div class="wrap">
+			<div class="top-head">
+				<h3>Services</h3>
+				<ul>
+					<li><a href="index.php">Home / </a></li>
+					<li><a href="./index.php?r=site%2Fservices"><span>Services</span></a></li>
+				</ul>
+			</div>
+			</div>
+			</div>
+
+		<div class="container">	
+			<div id="tabs" class="tabs">
+				<nav>
+					<ul>
+						<li><a href="#section-1" class="icon-immigrant"><span>Immigrant Visa</span></a></li>
+						<li><a href="#section-2" class="icon-non-immigrant"><span>Non-Immigrant Visa</span></a></li>
+						<li><a href="#section-3" class="icon-special"><span>Special Visa</span></a></li>
+					</ul>
+				</nav>
+				<div class="content">
+					<section id="section-1">
+						<div class="mediabox">
+							<h3>Immigrant Visa 1</h3>
+							<p>Sushi gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.</p>
+						</div>
+						<div class="mediabox">
+							<h3>Immigrant Visa 2</h3>
+							<p>Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea.</p>
+						</div>
+						<div class="mediabox">
+							<h3>Immigrant Visa 3</h3>
+							<p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+						</div>
+						<div class="mediabox">
+							<h3>Immigrant Visa 4</h3>
+							<p>Sushi gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.</p>
+						</div>
+						<div class="mediabox">
+							<h3>Immigrant Visa 5</h3>
+							<p>Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea.</p>
+						</div>
+						<div class="mediabox">
+							<h3>Immigrant Visa 6</h3>
+							<p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+						</div>
+					</section>
+					<section id="section-2">
+						<div class="mediabox">
+							<h3>Non-Immigrant Visa 1</h3>
+							<p>Chickweed okra pea winter purslane coriander yarrow sweet pepper radish garlic brussels sprout groundnut summer purslane earthnut pea tomato spring onion azuki bean gourd. </p>
+						</div>
+						<div class="mediabox">
+							<h3>Non-Immigrant Visa 2</h3>
+							<p>Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori. Grape wattle seed kombu beetroot horseradish carrot squash brussels sprout chard.</p>
+						</div>
+						<div class="mediabox">
+							<h3>Non-Immigrant Visa 3</h3>
+							<p>Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori. </p>
+						</div>
+					</section>
+					<section id="section-3">
+						<div class="mediabox">
+							<h3>Special Visa 1</h3>
+							<p>Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea.Sushi gumbo beet greens corn soko endive gumbo gourd.</p>
+						</div>
+						<div class="mediabox">
+							<h3>Special Visa 2</h3>
+							<p>Sushi gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.</p>
+						</div>
+						<div class="mediabox">
+							<h3>Special Visa 3</h3>
+							<p>Pea horseradish azuki bean lettuce avocado asparagus okra. Kohlrabi radish okra azuki bean corn fava bean mustard tigernut wasabi tofu broccoli mixture soup.</p>
+						</div>
+					</section>
+				</div><!-- /content -->
+			</div><!-- /tabs -->
+		</div>
+		<script src="js/cbpFWTabs.js"></script>
+		<script>
+			new CBPFWTabs( document.getElementById( 'tabs' ) );
+		</script>
