d7c673c338a2716afbd4d7ce18c626f5cb2cf6b3
Kayzelle Gabalfin <kayzellegabalfin@gmail.com>
2015-03-04 17:56:26 +0800

    Modified admin-panel page

---
+++ application/rbaconsultancy/views/site/admin-panel.php
@@ -0,0 +1,37 @@
+        
+            <div class="top-nav">
+                <ul>
+                    <li><a href="./index.php">Home</a></li>
+                    <li><a href="./index.php?r=site%2Fabout">About</a></li>
+                    <li><a href="./index.php?r=site%2Fservices">Services</a></li>
+                    <li><a href="./index.php?r=site%2Fcontact">Contact</a></li>
+                    <li class="active"><a href="./index.php?r=site%2Fadmin-panel">Admin's Panel</a></li>
+                </ul>
+            </div>
+   </div>
+</div>   
+
+            <link rel="stylesheet" type="text/css" href="css/sidebar.css" />
+            <script src="js/sidebar.js"></script>
+
+   <div id='cssmenu'>
+   <ul>
+      <li><a href='#'>Client's Account</a></li>
+      <li class='has-sub'><a href='#'>Visa Applications</a>
+         <ul>
+            <li><a href='#'>All</a></li>
+            <li><a href='#'>Pending</a></li>
+            <li><a href='#'>On-going</a></li>
+            <li><a href='#'>Cancelled/Denied</a></li>
+            <li><a href='#'>Completed</a></li>
+         </ul>
+      </li>
+      <li class='has-sub'><a href='#'>Services List</a>
+         <ul>
+            <li><a href='#'>Immigrant Visa</a></li>
+            <li><a href='#'>Non-immigrant Visa</a></li>
+            <li><a href='#'>Special Visa</a></li>
+         </ul>
+      </li>
+   </ul>
+   </div>
