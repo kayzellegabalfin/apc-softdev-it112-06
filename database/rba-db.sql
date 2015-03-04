b003ee88197bbe4a98897ea1fec04ef26674fc74
Kayzelle Gabalfin <kayzellegabalfin@gmail.com>
2015-03-04 12:00:21 +0800

    Updated foreign key referential action to Cascade

--- database/rba-db.sql
+++ database/rba-db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 17, 2015 at 01:44 PM
+-- Generation Time: Mar 04, 2015 at 04:47 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -50,16 +50,6 @@ CREATE TABLE IF NOT EXISTS `requirements` (
   `req_datefileuploaded` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `requirements`
---
-
-INSERT INTO `requirements` (`req_id`, `user_id`, `slist_id`, `rlist_id`, `req_status`, `req_fileuploaded`, `req_datefileuploaded`) VALUES
-('34t87yfu', 1, 1, 3, 'no file submitted', NULL, NULL),
-('487tyrny', 1, 1, 3, 'no file submitted', NULL, NULL),
-('7t6rbg7e', 1, 2, 4, 'no file submitted', NULL, NULL),
-('ertyubvy', 1, 2, 4, 'no file submitted', NULL, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -71,14 +61,6 @@ CREATE TABLE IF NOT EXISTS `requirements_list` (
   `rlist_name` varchar(50) NOT NULL,
   `rlist_desc` text
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `requirements_list`
---
-
-INSERT INTO `requirements_list` (`rlist_id`, `rlist_name`, `rlist_desc`) VALUES
-(3, 'Birth Certificate', NULL),
-(4, 'Request Letter', NULL);
 
 -- --------------------------------------------------------
 
@@ -106,14 +88,6 @@ CREATE TABLE IF NOT EXISTS `service_list` (
   `slist_desc` text
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `service_list`
---
-
-INSERT INTO `service_list` (`slist_id`, `slist_name`, `slist_desc`) VALUES
-(1, 'Change of Status', NULL),
-(2, 'Extension', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -139,13 +113,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_age` int(2) DEFAULT NULL,
   `user_type` varchar(15) NOT NULL DEFAULT 'client'
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `user_email`, `user_password`, `user_lname`, `user_fname`, `user_mname`, `user_housenum`, `user_street`, `user_city`, `user_country`, `user_postalcode`, `user_gender`, `user_contact`, `user_company`, `user_birthdate`, `user_age`, `user_type`) VALUES
-(1, 'kayzellegabalfin@gmail.com', 'samplepassword', 'Gabalfin', 'Kayzelle', 'Cabaysa', '26 - B', 'Talaba 5', 'Bacoor', 'Philippines', 4107, 'Female', '092222222', 'Asia Pacific College', NULL, NULL, 'client');
 
 --
 -- Indexes for dumped tables
@@ -214,22 +181,22 @@ MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- Constraints for table `price_list`
 --
 ALTER TABLE `price_list`
-ADD CONSTRAINT `price_list_fk_1` FOREIGN KEY (`plist_id`) REFERENCES `service_list` (`slist_id`);
+ADD CONSTRAINT `price_list_fk_1` FOREIGN KEY (`plist_id`) REFERENCES `service_list` (`slist_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `requirements`
 --
 ALTER TABLE `requirements`
-ADD CONSTRAINT `requirements_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-ADD CONSTRAINT `requirements_fk_2` FOREIGN KEY (`slist_id`) REFERENCES `service_list` (`slist_id`),
-ADD CONSTRAINT `requirements_fk_3` FOREIGN KEY (`rlist_id`) REFERENCES `requirements_list` (`rlist_id`);
+ADD CONSTRAINT `requirements_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `requirements_fk_2` FOREIGN KEY (`slist_id`) REFERENCES `service_list` (`slist_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `requirements_fk_3` FOREIGN KEY (`rlist_id`) REFERENCES `requirements_list` (`rlist_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `services`
 --
 ALTER TABLE `services`
-ADD CONSTRAINT `services_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-ADD CONSTRAINT `services_fk_2` FOREIGN KEY (`slist_id`) REFERENCES `service_list` (`slist_id`);
+ADD CONSTRAINT `services_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `services_fk_2` FOREIGN KEY (`slist_id`) REFERENCES `service_list` (`slist_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
