6c7046d5c83f451e0a9a6d01e1b75305224c2989
Jessica Martinez <martinezjecca@gmail.com>
2015-03-07 19:11:07 +0800

    Update fields slist_id and rlist_id in the form for requirementperservice to show the Service List Name and Requirements List Name as a drop down

--- application/rbaconsultancy_adv/backend/views/requirementsperservice/_form.php
+++ application/rbaconsultancy_adv/backend/views/requirementsperservice/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\servicelist;
+use common\models\requirementslist;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\RequirementsPerService */
@@ -16,9 +19,25 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'rps_name')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'slist_id')->textInput() ?>
+   	<?php
+			$servicelist=servicelist::find()->all();
+	
+		$listData=ArrayHelper::map($servicelist,'slist_id','slist_name');
 
-    <?= $form->field($model, 'rlist_id')->textInput() ?>
+			echo $form->field($model, 'slist_id')->dropDownList(
+								$listData,
+									['prompt'=>'Select Service Name...']);
+		?>
+
+    <?php
+			$requirementslist=requirementslist::find()->all();
+	
+		$listData=ArrayHelper::map($requirementslist,'rlist_id','rlist_name');
+
+			echo $form->field($model, 'rlist_id')->dropDownList(
+								$listData,
+									['prompt'=>'Select Requirements Name...']);
+		?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
